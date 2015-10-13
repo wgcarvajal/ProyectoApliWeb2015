06147840d514d8d5b831ce6891d7c12b8012dadc
Wilson Carvajal <wgcarvajal@unicauca.edu.co>
2015-10-13 16:22:16 -0500

    inicio de sesion con glassfish

--- src/java/com/unicauca/apliweb/managedbean/personas/UsuariosController.java
+++ src/java/com/unicauca/apliweb/managedbean/personas/UsuariosController.java
@@ -5,23 +5,94 @@
  */
 package com.unicauca.apliweb.managedbean.personas;
 
+import java.io.IOException;
+import javax.faces.application.Application;
+import javax.faces.application.ViewHandler;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import org.primefaces.context.RequestContext;
 
 /**
  *
  * @author geovanny
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class UsuariosController
 {
+    private String nombreUsuario;
+    private String contrasena;
 
-    /**
-     * Creates a new instance of UsuariosController
-     */
+   
     public UsuariosController() 
     {
     }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) 
+    {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() 
+    {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena)
+    {
+        this.contrasena = contrasena;
+    }
+    
+    
+     public void login()throws IOException, ServletException 
+    {
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+        FacesContext fc = FacesContext.getCurrentInstance();
+        HttpServletRequest req = (HttpServletRequest) fc.getExternalContext().getRequest();        
+        if (req.getUserPrincipal() == null) {
+            try 
+            {
+                req.login(this.nombreUsuario, this.contrasena);
+                req.getServletContext().log("Autenticacion exitosa");
+                
+            } 
+            catch (ServletException e) 
+            {
+                //fc.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Nombre de usuario o contraseña incorrectos", "Nombre de usuario o contraseña incorrectos"));
+                requestContext.update("formularioInicioSession");                
+            }
+        } 
+        else 
+        {
+            req.getServletContext().log("El usuario ya estaba logueado:  ");
+            requestContext.update("formularioInicioSession");
+        }
+    }
+    
+    
+    public void ventanaInicioSession()
+    {
+       RequestContext requestContext = RequestContext.getCurrentInstance();          
+       FacesContext context = FacesContext.getCurrentInstance();
+       Application application = context.getApplication();
+       ViewHandler viewHandler = application.getViewHandler();
+       UIViewRoot viewRoot = viewHandler.createView(context, context.getViewRoot().getViewId());
+       context.setViewRoot(viewRoot);       
+       context.renderResponse();
+       this.contrasena=null;
+       this.nombreUsuario=null;
+       requestContext.update("formularioInicioSession");       
+       requestContext.execute("PF('IniciarSesion').show()");
+    }
+    
+    
     
 }
